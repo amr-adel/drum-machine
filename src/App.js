f700b167568f92ca7cf2104894cdee4ec643ccc7
Amr A. Abdelmoez <amradel88@live.com>
2019-08-02 22:06:19 +0200

    feat: Add footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,36 @@ function App() {
         <h1>drum machine</h1>
       </main>
       <footer className='footer'>
-        <h1>footer</h1>
+        <div className='centered'>
+          <aside className='info'>
+            <p>
+              For FreeCodeCamp |{' '}
+              <a href='https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-drum-machine/' target='_blank' rel='noopener noreferrer'>
+                Drum Machine Challenge
+              </a>
+            </p>
+            <p>
+              By{' '}
+              <a href='https://github.com/amr-adel' target='_blank' rel='noopener noreferrer'>
+                Fullstack Amr
+              </a>
+            </p>
+          </aside>
+          <aside className='credit'>
+            <p>
+              Background from{' '}
+              <a href='https://www.toptal.com/designers/subtlepatterns/random-grey-variations/' target='_blank' rel='noopener noreferrer'>
+                Subtle Patterns
+              </a>
+            </p>
+            <p>
+              Drum Samples from{' '}
+              <a href='ttp://99sounds.org/drum-samples/' target='_blank' rel='noopener noreferrer'>
+                99Sounds
+              </a>
+            </p>
+          </aside>
+        </div>
       </footer>
     </>
   )
