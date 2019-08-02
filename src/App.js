0929e2d897c8642b7c9b1bfa128542a8e8f42799
Amr A. Abdelmoez <amradel88@live.com>
2019-08-02 20:53:11 +0200

    feat: Basic layout

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Drum Machine</h1>
-    </div>
+    <>
+      <header className='header'>
+        <h1>header</h1>
+      </header>
+      <main id='drum-machine'>
+        <h1>drum machine</h1>
+      </main>
+      <footer className='footer'>
+        <h1>footer</h1>
+      </footer>
+    </>
   )
 }
 
